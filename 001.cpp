467f339ae634a306f8656a86009479ed4f42b718
jolloween <jp-pedrosa@hotmail.com>
2025-02-15 17:03:35 +0000

    Create 001.cpp

---
+++ 001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string nome;
+int idade;
+    cout << "Qual seu nome?" << endl;
+    getline (cin, nome);
+cout << "Qual a idade? " << endl;
+cin >> idade;
+
+ cout << nome << endl;
+ cout << idade;
+    return 0;
+}
